e166d2a0be04eb2131330117840030fb8ca7d654
PedroRedondo <pedroignacioredondo1@gmail.com>
2024-05-08 20:06:12 -0300

    2° Parte Homework Modulo 6
    2 ° parte de soluciones a los ejercicios del Modulo 6 Bucles de Js.

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,18 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  let producto = 1;
+  if (a <= b ){
+    for (let i = a; i <= b; i ++){
+      producto = producto * i;
+    }
+  } else {
+    for (let i = b; i <= a; i++){
+      producto = producto * i ;
+    }
+  }
+  return producto;
 }
+
 
 module.exports = productoEntreNúmeros;
--- C6 Bucles/ejercicios/19.js
+++ C6 Bucles/ejercicios/19.js
@@ -2,6 +2,11 @@ function sumarHastaN(n) {
   // La función recibe un número "n" por argumento.
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Tu código:
+  let cero = 0
+  for(let i = 1; i <= n; i++){
+    cero = cero + i;
+  }
+  return cero;
 }
 
 module.exports = sumarHastaN;
